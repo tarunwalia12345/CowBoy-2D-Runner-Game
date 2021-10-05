3f63e4d56c2d7f603afa4f46c1b3cdc445559858
tarunwalia12345 <57227656+tarunwalia12345@users.noreply.github.com>
2021-10-05 10:07:01 +0530

    Cowboy Runner

---
+++ Assets/Scripts/Background Scripts/BGScaler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BGScaler : MonoBehaviour {
+	
+	void Start () {
+		var height = Camera.main.orthographicSize * 2f;
+		var width = height * Screen.width / Screen.height;
+
+		if (gameObject.name == "Background") {
+			transform.localScale = new Vector3 (width, height, -2);
+		} else {
+			transform.localScale = new Vector3 (width + 4f, 5, -2);
+		}
+
+	}
+
+}
